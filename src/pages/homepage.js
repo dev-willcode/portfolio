51538de69272d6be884d03898170d22376437339
Sayan Mondal <sayanmondal342@gmail.com>
2020-06-20 21:54:30 +0530

    Added Terminal

---
+++ src/pages/homepage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "../styles/homepage.module.css"
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is supposed to be a HomePage</h1>
+      <p>Don't Judge me because of this</p>
+    </div>
+  )
+}
+
+export default Home
