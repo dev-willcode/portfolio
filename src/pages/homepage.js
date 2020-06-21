2ca94a55f69ec6d7e9a17c9271a25812b699af8c
Sayan Mondal <sayanmondal342@gmail.com>
2020-06-22 01:25:58 +0530

    Fixed Sidebar Nav

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -7,6 +7,12 @@ const Home = () => {
     <Layout header={"homepage broo"}>
       <h1>This is supposed to be a HomePage</h1>
       <p>Don't Judge me because of this</p>
+      <p>Don't Judge me because of this</p>
+      <p>Don't Judge me because of this</p>
+      <p>Don't Judge me because of this</p>
+      <p>Don't Judge me because of this</p>
+      <p>Don't Judge me because of this</p>v
+      <p>Don't Judge me because of this</p>
     </Layout>
   )
 }
