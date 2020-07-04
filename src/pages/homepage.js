28840ee016727dea7d149fc35ec73627c467dfd0
Sayan Mondal <sayan.mondal@mayadata.io>
2020-07-04 22:46:17 +0530

    Configured header

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,18 +3,7 @@ import Layout from '../components/layout'
 import '../styles/homepage.module.css'
 
 const Home = () => {
-  return (
-    <Layout header={'homepage broo'}>
-      <h1>This is supposed to be a HomePage</h1>
-      <p>Don't Judge me because of this</p>
-      <p>Don't Judge me because of this</p>
-      <p>Don't Judge me because of this</p>
-      <p>Don't Judge me because of this</p>
-      <p>Don't Judge me because of this</p>
-      <p>Don't Judge me because of this</p>v
-      <p>Don't Judge me because of this</p>
-    </Layout>
-  )
+  return <Layout header={'Home'}></Layout>
 }
 
 export default Home
